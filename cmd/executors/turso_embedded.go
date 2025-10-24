53de4557d29d1955d6285341d365f18ff272b94f
cyw0ng <chunyu.wang.1995@connect.hku.hk>
2025-10-24 21:20:28 +0800

    Add initial project files: .gitignore, go.mod, go.sum, and turso_embedded.go
    Signed-off-by: cyw0ng <chunyu.wang.1995@connect.hku.hk>

---
+++ cmd/executors/turso_embedded.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"os"
+
+	_ "github.com/tursodatabase/go-libsql"
+)
+
+type User struct {
+	ID   int
+	Name string
+}
+
+func queryUsers(db *sql.DB) {
+	rows, err := db.Query("SELECT * FROM users")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "failed to execute query: %v\n", err)
+		os.Exit(1)
+	}
+	defer rows.Close()
+
+	var users []User
+
+	for rows.Next() {
+		var user User
+
+		if err := rows.Scan(&user.ID, &user.Name); err != nil {
+			fmt.Println("Error scanning row:", err)
+			return
+		}
+
+		users = append(users, user)
+		fmt.Println(user.ID, user.Name)
+	}
+
+	if err := rows.Err(); err != nil {
+		fmt.Println("Error during rows iteration:", err)
+	}
+}
+
+func main() {
+	dbName := "file:./local.db"
+
+	db, err := sql.Open("libsql", dbName)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "failed to open db %s", err)
+		os.Exit(1)
+	}
+	defer db.Close()
+}
