7b1f3057bdc4749ce1d2316467fb7abd211a9a8c
cyw0ng <chunyu.wang.1995@connect.hku.hk>
2025-10-24 22:39:21 +0800

    Refactor query execution to use goroutines for concurrent processing
    Signed-off-by: cyw0ng <chunyu.wang.1995@connect.hku.hk>

--- cmd/executors/turso_embedded.go
+++ cmd/executors/turso_embedded.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"os"
 	"sqlsmith-go/internal/generators/turso"
+	"sync"
 
 	_ "github.com/tursodatabase/turso-go"
 )
@@ -15,14 +16,37 @@ func main() {
 		fmt.Printf("Error: %v\n", err)
 		os.Exit(1)
 	}
+	defer conn.Close()
 
-	for i := 0; i < 100; i++ {
-		gen := turso.NewGenerator(uint64(i + 1))
-		query := gen.Generate()
-		fmt.Printf("Executing query: %s\n", query)
-		_, err := conn.Exec(query)
-		if err != nil {
-			fmt.Printf("Error executing query: %v\n", err)
-		}
+	const (
+		numWorkers       = 8
+		queriesPerWorker = 100
+	)
+
+	var wg sync.WaitGroup
+	wg.Add(numWorkers)
+
+	errCh := make(chan error, numWorkers*queriesPerWorker)
+
+	for w := 0; w < numWorkers; w++ {
+		go func(workerID int) {
+			defer wg.Done()
+			for i := 0; i < queriesPerWorker; i++ {
+				gen := turso.NewGenerator(uint64(workerID*queriesPerWorker + i + 1))
+				query := gen.Generate()
+				fmt.Printf("[worker %d] Executing query: %s\n", workerID, query)
+				_, err := conn.Exec(query)
+				if err != nil {
+					errCh <- fmt.Errorf("[worker %d] Error executing query: %v", workerID, err)
+				}
+			}
+		}(w)
+	}
+
+	wg.Wait()
+	close(errCh)
+
+	for err := range errCh {
+		fmt.Println(err)
 	}
 }
